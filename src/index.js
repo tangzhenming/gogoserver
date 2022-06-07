ae19abab788ee1040306527d8ced96f6acb208fc
tang <15977536060@163.com>
2022-06-07 13:29:10 +0800

    feat: 支持指定端口号

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
+const { Command } = require("commander");
+const http = require("http");
 
-console.log("gogoserver!");
+function startService(port) {
+  const server = http.createServer((req, res) => {});
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+const program = new Command();
+program.name("gogoserver").description("CLI to start a server");
+program
+  .command("serve")
+  .description("start service")
+  .option("-p, --port <number>", 3000)
+  .action(({ port }) => {
+    startService(port);
+  });
+program.parse(process.argv);
