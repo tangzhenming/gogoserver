2b193e344d10240938133a86bce557dc33b319be
tang <15977536060@163.com>
2022-06-08 17:06:42 +0800

    refactor: use fs promise module

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@
 const { Command } = require("commander");
 const http = require("http");
 const fs = require("fs");
+const fsp = require("fs").promises;
 const path = require("path");
 const template = require("art-template");
 
 const server = http.createServer();
 let port;
 
-function startService(userPath) {
+async function startService(userPath) {
   const cwd = process.cwd();
   const completePath = path.join(cwd, userPath);
-  const stats = fs.statSync(completePath);
+  const stats = await fsp.stat(completePath);
 
   if (stats.isDirectory()) {
     // --path 指定目录
-    const allFiles = fs.readdirSync(completePath);
+    const allFiles = await fsp.readdir(completePath);
     const isEmptyDir = allFiles.length === 0;
     const fileData = allFiles.map((file) => {
       return {
@@ -24,20 +25,19 @@ function startService(userPath) {
       };
     });
 
-    server.on("request", (req, res) => {
-      fs.readFile(path.join(__dirname, "./index.html"), (err, data) => {
-        if (err || isEmptyDir) return res.end("404");
-        const htmlStr = template.render(data.toString(), {
-          fileData,
-        });
-        res.end(htmlStr);
+    server.on("request", async (req, res) => {
+      const html = await fsp.readFile(path.join(__dirname, "./index.html"));
+      if (!html || isEmptyDir) return res.end("404");
+      const htmlStr = template.render(html.toString(), {
+        fileData,
       });
+      res.end(htmlStr);
     });
   } else {
     // --path 指定文件
     const fileSizeInBytes = stats.size;
     server.on("request", (req, res) => {
-      const stream = fs.createReadStream(completePath, "utf8");
+      const stream = fs.createReadStream(completePath);
       res.writeHead(200, {
         "Content-Length": fileSizeInBytes,
       });
@@ -53,12 +53,12 @@ program
   .description("start service")
   .option("--port <number>", "port to start server on")
   .option("--path <path>", "path to serve")
-  .action(({ port: userPort, path: userPath }) => {
+  .action(async ({ port: userPort, path: userPath }) => {
     port = userPort || 3000;
     startService(userPath || "");
-  });
-program.parse(process.argv);
+  })
+  .parse(process.argv);
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
